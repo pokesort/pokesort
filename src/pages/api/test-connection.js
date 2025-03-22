1730dc63930392ec56d21b7bee5ddf3b47788ff9
bunnysammy_ <davidenacrene@gmail.com>
2025-03-22 15:36:22 -0300

    :sparkles: feat: Conexão com banco de dados

---
+++ src/pages/api/test-connection.js
@@ -0,0 +1,17 @@
+import { connect, data } from "@/lib/mongodb";
+
+export default async function handler(req, res) {
+  try {
+    await connect();
+
+    console.log(data);
+    const collections = await data.db.listCollections().toArray();
+
+    res.status(200).json({ success: true, collections: collections.map((col) => col.name) });
+  } catch (error) {
+    console.error("Connection failed:", error);
+    res.status(500).json({ success: false, error: error.message });
+  } finally {
+
+  }
+}
