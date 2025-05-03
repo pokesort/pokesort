975534190652790c7473614a25a34fe64e5b262e
bunnysammy_ <davidenacrene@gmail.com>
2025-05-03 20:14:49 -0300

    :bug: fix: Importação do model de puzzle

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
+const path = require('path');
 
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   },
 };
 
