2ae4050c829de45da7d9c92fa57996601c9bce18
welsondeivid <53789831+welsondeivid@users.noreply.github.com>
2025-07-01 15:54:00 -0300

    :bug: fix: Dex para megas e gmax

--- src/pages/api/pokemon/dex.js
+++ src/pages/api/pokemon/dex.js
@@ -21,261 +21,287 @@ export default async function handler(req, res) {
   } finally {
   }
 
-  async function getDexData(id_dex) {
-    const pokemon = await data.db
-      .collection("pokemon")
-      .aggregate([
-        {
-          $match: {
-            $or: [{ id: Number(id_dex) }, { name: id_dex }],
-          },
+  function matchPokemonStage(id_dex) {
+    return [
+      {
+        $match: {
+          $or: [{ id: Number(id_dex) }, { name: id_dex }],
         },
-        {
-          $lookup: {
-            from: "evolution_steps",
-            localField: "evolution_step",
-            foreignField: "id",
-            as: "evo_data",
-            pipeline: [
-              {
-                $project: {
-                  _id: 0,
-                  id: 1,
-                },
+      },
+    ];
+  }
+
+  function enrichWithEvolutionStep() {
+    return [
+      {
+        $lookup: {
+          from: "evolution_steps",
+          localField: "evolution_step",
+          foreignField: "id",
+          as: "evo_data",
+          pipeline: [{ $project: { _id: 0, id: 1 } }],
+        },
+      },
+      { $unwind: "$evo_data" },
+      {
+        $addFields: {
+          split_parts: { $split: ["$evo_data.id", "-"] },
+        },
+      },
+      {
+        $addFields: {
+          chain_id: {
+            $cond: {
+              if: { $eq: [{ $size: "$split_parts" }, 3] },
+              then: {
+                $concat: [
+                  { $arrayElemAt: ["$split_parts", 0] },
+                  "-",
+                  { $arrayElemAt: ["$split_parts", 1] },
+                ],
               },
-            ],
-          },
-        },
-        { $unwind: "$evo_data" },
-        {
-          $addFields: {
-            split_parts: { $split: ["$evo_data.id", "-"] },
-          },
-        },
-        {
-          $addFields: {
-            chain_id: {
-              $cond: {
-                if: { $eq: [{ $size: "$split_parts" }, 3] },
-                then: {
-                  $concat: [
-                    { $arrayElemAt: ["$split_parts", 0] },
-                    "-",
-                    { $arrayElemAt: ["$split_parts", 1] },
-                  ],
-                },
-                else: { $arrayElemAt: ["$split_parts", 0] },
-              },
+              else: { $arrayElemAt: ["$split_parts", 0] },
             },
           },
         },
-        {
-          $lookup: {
-            from: "pokemon",
-            let: { chainId: "$chain_id" },
-            pipeline: [
-              {
-                $match: {
-                  $expr: {
-                    $regexMatch: {
-                      input: "$evolution_step",
-                      regex: { $concat: ["^", "$$chainId", "-"] },
-                    },
+      },
+    ];
+  }
+
+  function getEvolutionChain() {
+    return [
+      {
+        $lookup: {
+          from: "pokemon",
+          let: { chainId: "$chain_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $regexMatch: {
+                    input: "$evolution_step",
+                    regex: { $concat: ["^", "$$chainId", "-"] },
                   },
                 },
               },
-            ],
-            as: "chainData",
-          },
+            },
+          ],
+          as: "chainData",
         },
-        {
-          $addFields: {
-            chain: {
-              $map: {
-                input: "$chainData",
-                as: "cd",
-                in: {
-                  name: "$$cd.name",
-                  id: "$$cd.id",
-                  species_name: "$$cd.species_name",
-                  dex_number: "$$cd.dex_number",
-                  evolution_step: "$$cd.evolution_step",
-                },
+      },
+      {
+        $addFields: {
+          chain: {
+            $map: {
+              input: "$chainData",
+              as: "cd",
+              in: {
+                name: "$$cd.name",
+                id: "$$cd.id",
+                species_name: "$$cd.species_name",
+                dex_number: "$$cd.dex_number",
+                evolution_step: "$$cd.evolution_step",
               },
             },
           },
         },
-        {
-          $lookup: {
-            from: "evolution_steps",
-            localField: "chain.evolution_step",
-            foreignField: "id",
-            as: "chain_evo_data",
-            pipeline: [
-              {
-                $project: {
-                  _id: 0,
-                  id: 1,
-                  methods: 1,
-                  pokemon: 1
-                },
-              },
-            ],
-          },
+      },
+      {
+        $lookup: {
+          from: "evolution_steps",
+          localField: "chain.evolution_step",
+          foreignField: "id",
+          as: "chain_evo_data",
+          pipeline: [{ $project: { _id: 0, id: 1, methods: 1, pokemon: 1 } }],
         },
-        {
-          $addFields: {
-            chain: {
-              $map: {
-                input: "$chain",
-                as: "c",
-                in: {
-                  $mergeObjects: [
-                    "$$c",
-                    {
-                      methods: {
-                        $let: {
-                          vars: {
-                            matched: {
-                              $filter: {
-                                input: "$chain_evo_data",
-                                as: "ced",
-                                cond: {
-                                  $and: [
-                                    { $eq: ["$$ced.id", "$$c.evolution_step"] },
-                                    { $eq: ["$$ced.pokemon", "$$c.id"] },
-                                  ],
-                                },
+      },
+      {
+        $addFields: {
+          chain: {
+            $map: {
+              input: "$chain",
+              as: "c",
+              in: {
+                $mergeObjects: [
+                  "$$c",
+                  {
+                    methods: {
+                      $let: {
+                        vars: {
+                          matched: {
+                            $filter: {
+                              input: "$chain_evo_data",
+                              as: "ced",
+                              cond: {
+                                $and: [
+                                  { $eq: ["$$ced.id", "$$c.evolution_step"] },
+                                  { $eq: ["$$ced.pokemon", "$$c.id"] },
+                                ],
                               },
                             },
                           },
-                          in: {
-                            $ifNull: [
-                              { $arrayElemAt: ["$$matched.methods", 0] },
-                              [],
-                            ],
-                          },
+                        },
+                        in: {
+                          $ifNull: [{ $arrayElemAt: ["$$matched.methods", 0] }, []],
                         },
                       },
                     },
-                  ],
-                },
+                  },
+                ],
               },
             },
           },
         },
-        {
-          $lookup: {
-            from: "moves",
-            localField: "moves",
-            foreignField: "id",
-            as: "movesData",
-            pipeline: [
-              {
-                $project: {
-                  _id: 0,
-                  id: 1,
-                  name: 1,
-                  type_id: 1,
-                },
-              },
-            ],
+      },
+    ];
+  }
+
+  function enrichWithMoves() {
+    return [
+      {
+        $lookup: {
+          from: "moves",
+          localField: "moves",
+          foreignField: "id",
+          as: "movesData",
+          pipeline: [{ $project: { _id: 0, id: 1, name: 1, type_id: 1 } }],
+        },
+      },
+      {
+        $addFields: {
+          moves: {
+            $map: {
+              input: "$movesData",
+              as: "m",
+              in: { name: "$$m.name", type: "$$m.type_id" },
+            },
           },
         },
-        {
-          $addFields: {
-            moves: {
-              $map: {
-                input: "$movesData",
-                as: "m",
-                in: { name: "$$m.name", type: "$$m.type_id" },
+      },
+    ];
+  }
+
+  function enrichWithAbilities() {
+    return [
+      {
+        $lookup: {
+          from: "abilities",
+          localField: "abilities",
+          foreignField: "id",
+          as: "abilitiesData",
+          pipeline: [{ $project: { _id: 0, id: 1, name: 1 } }],
+        },
+      },
+      {
+        $addFields: {
+          abilities: {
+            $map: { input: "$abilitiesData", as: "a", in: "$$a.name" },
+          },
+        },
+      },
+    ];
+  }
+
+  function enrichWithForms() {
+    return [
+      {
+        $addFields: {
+          otherFormsInt: {
+            $map: {
+              input: "$other_forms",
+              as: "f",
+              in: { $toInt: "$$f" },
+            },
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "pokemon",
+          localField: "otherFormsInt",
+          foreignField: "id",
+          as: "formsData",
+          pipeline: [
+            {
+              $project: {
+                _id: 0,
+                id: 1,
+                name: 1,
+                species_name: 1,
+                dex_number: 1,
+              },
+            },
+          ],
+        },
+      },
+      {
+        $addFields: {
+          other_forms: {
+            $map: {
+              input: "$formsData",
+              as: "f",
+              in: {
+                name: "$$f.name",
+                id: "$$f.id",
+                species_name: "$$f.species_name",
+                dex_number: "$$f.dex_number",
               },
             },
           },
         },
-        {
-          $lookup: {
-            from: "abilities",
-            localField: "abilities",
-            foreignField: "id",
-            as: "abilitiesData",
-            pipeline: [
-              {
-                $project: {
-                  _id: 0,
-                  id: 1,
-                  name: 1,
-                },
-              },
-            ],
-          },
+      },
+    ];
+  }
+
+  function cleanupFields() {
+    return [
+      {
+        $project: {
+          _id: 0,
+          movesData: 0,
+          abilitiesData: 0,
+          formsData: 0,
+          otherFormsInt: 0,
+          chain_id: 0,
+          chainData: 0,
+          chain_evo_data: 0,
+          split_parts: 0,
+          evo_data: 0,
+          evolution_step: 0,
         },
-        {
-          $addFields: {
-            abilities: {
-              $map: { input: "$abilitiesData", as: "a", in: "$$a.name" },
-            },
-          },
-        },
-        {
-          $addFields: {
-            otherFormsInt: {
-              $map: {
-                input: "$other_forms",
-                as: "f",
-                in: { $toInt: "$$f" },
-              },
-            },
-          },
-        },
-        {
-          $lookup: {
-            from: "pokemon",
-            localField: "otherFormsInt",
-            foreignField: "id",
-            as: "formsData",
-            pipeline: [
-              {
-                $project: {
-                  _id: 0,
-                  id: 1,
-                  name: 1,
-                  species_name: 1,
-                  dex_number: 1
-                },
-              },
-            ],
-          },
-        },
-        {
-          $addFields: {
-            other_forms: {
-              $map: {
-                input: "$formsData",
-                as: "f",
-                in: { name: "$$f.name", id: "$$f.id", species_name: "$$f.species_name", dex_number: "$$f.dex_number"},
-              },
-            },
-          },
-        },
-        {
-          $project: {
-            _id: 0,
-            movesData: 0,
-            abilitiesData: 0,
-            formsData: 0,
-            otherFormsInt: 0,
-            chain_id: 0,
-            chainData: 0,
-            chain_evo_data: 0,
-            split_parts: 0,
-            evo_data: 0,
-            evolution_step: 0,
-          },
-        },
-      ])
+      },
+    ];
+  }
+
+  async function hasEvolutionStep(id_dex) {
+    const poke = await data.db.collection("pokemon").findOne(
+      { $or: [{ id: Number(id_dex) }, { name: id_dex }] },
+      { projection: { evolution_step: 1 } }
+    );
+    return poke?.evolution_step != null;
+  }
+
+  // Função principal
+  async function getDexData(id_dex) {
+
+    const hasEvo = await hasEvolutionStep(id_dex);
+
+    const pipeline = [
+      ...matchPokemonStage(id_dex),
+      ...(hasEvo ? enrichWithEvolutionStep() : []),
+      ...(hasEvo ? getEvolutionChain() : []),
+      ...enrichWithMoves(),
+      ...enrichWithAbilities(),
+      ...enrichWithForms(),
+      ...cleanupFields(),
+    ];
+
+    const result = await data.db
+      .collection("pokemon")
+      .aggregate(pipeline)
       .toArray();
 
-    return pokemon[0];
+    return result[0];
   }
+
 }
