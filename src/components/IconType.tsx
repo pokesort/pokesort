fd5717e652cee2cafbdd036ab27dc759d9dc418d
bunnysammy_ <davidenacrene@gmail.com>
2025-07-02 11:50:53 -0300

    :construction: feat: Informações da dex

--- src/components/IconType.tsx
+++ src/components/IconType.tsx
@@ -1,15 +1,18 @@
 import '@/src/styles/components/IconType.scss';
+import React from 'react';
 
 interface IconTypeProps {
     folder: string;
     item: string;
 }
 
-export default function IconType({ folder, item }: IconTypeProps) {
+export default React.memo(function IconType({ folder, item }: IconTypeProps) {
     
     return (
         <div className={`type-icon ${folder}-${item}`}>
-            <img src={`/icons/${folder}/${item}.png`} />
+            {folder != 'color' &&
+                <img src={`/icons/${folder}/${item}.png`} />
+            }
         </div>
     )
-}
+})
