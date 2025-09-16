1ae78c4bfa12f035bbcd9f17e81801fd2da31c47
bunnysammy_ <davidenacrene@gmail.com>
2025-09-16 09:55:28 -0300

    :construction: feat: Ícones no header

---
+++ src/components/svg/HeaderTwitter.tsx
@@ -0,0 +1,7 @@
+export default function HeaderTwitter () {
+    return (
+        <svg width="15" height="15" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0.486359 0.52002L6.64444 9.49752L0.616788 16.48H1.95226L7.23425 10.3572L11.4342 16.48H16.48L10.0463 7.10139L15.7237 0.52002H14.3923L9.45566 6.23924L5.5321 0.52002H0.486359ZM2.08105 1.36002H5.08995L14.8853 15.64H11.8764L2.08105 1.36002Z" fill="currentColor"/>
+        </svg>
+    )
+}
