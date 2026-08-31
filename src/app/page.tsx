25bd9f76c47679cde9ca21b47bbf6e4996734831
bunny-sammy <dsamuel.coelho@hotmail.com>
2026-08-31 17:21:26 -0300

    :construction: feat: Nova tela home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 "use client"
 
-import { useTranslations } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
 import { redirect } from 'next/navigation';
 
 import "@/src/styles/components/Home.scss";
 import SvgLogo from '../components/svg/SvgLogo';
 import Link from 'next/link';
-import GridIcon from '../components/svg/GridIcon';
-import DexIcon from '../components/svg/DexIcon';
-import CalendarIcon from '../components/svg/CalendarIcon';
 
 import StreakIcon from '@/src/components/svg/StreakIcon';
 import TickIcon from '@/src/components/svg/TickIcon';
 import { useEffect, useState } from 'react';
+import PokeSprite from '../components/PokeSprite';
 
 const streakKey = 'u_dailystreak';
 const infiniteCount = 'u_infinitecount';
@@ -25,82 +23,140 @@ type Page = {
     countType?: 'streak' | 'count';
 }
 
+interface HomeCardProps {
+  page: string;
+  href: string;
+  sprite: string;
+  count?: number;
+}
+
+const HomeCard = ({
+  page, href, sprite, count=0
+}: HomeCardProps) => {
+  const t = useTranslations();
+  const locale = useLocale();
+  let commandAppend = "";
+  let extraElement = null;
+
+  switch (page) {
+    case "daily":
+      commandAppend = new Date().toLocaleDateString(locale, {
+        day: '2-digit',
+        month: 'long'
+      });
+    break;
+    case "infinite":
+      extraElement = (
+        <p>Weekly archive goes here</p>
+      )
+    break;
+  }
+
+  return (
+    <div className="home-card">
+      <h3>
+        {t(`header.${page}`)}
+        {count > 1 &&
+          <span>
+            {count}
+          </span>
+        }
+      </h3>
+      <p>{t(`home.${page}.description`)}</p>
+
+      {extraElement}
+
+      <Link href={href}>
+        {t(`home.${page}.command`)}{commandAppend}
+      </Link>
+
+      <PokeSprite slug={`${sprite}.png`} />
+    </div>
+  )
+}
+
 export default function Home() {
   const t = useTranslations();
-  const [counts, setCounts] = useState<number[]>([]);
-
-  const pages = [
-      {route: '/daily', label: t('header.daily'), description: t('home.daily'), icon: <GridIcon/>, countType: 'streak'},
-      {route: '/infinite', label: t('header.infinite'), description: t('home.infinite'), icon: <GridIcon/>, countType: 'count'},
-      {route: '/archive', label: t('header.archive'), description: t('home.archive'), icon: <CalendarIcon/>},
-      {route: '/dex', label: t('header.dex'), description: t('home.dex'), icon: <DexIcon/>},
-  ] as Page[];
+  const [counts, setCounts] = useState<Record<string, number>>({"daily": 0, "infinite": 0});
+  const [dailySprite, setDailySprite] = useState<string>("151");
+  const [archiveSprite, setArchiveSprite] = useState<string>("251");
 
   useEffect(() => {
-    const newCounts = [...counts];
+    const newCounts = {...counts};
 
-    pages.map((page: Page, index: number) => {
+    Object.keys(newCounts).forEach(mode => {
       let streak = 0;
-      if (page.countType != undefined) {
 
-        if (page.countType != 'count') {
-            const streakJson = localStorage.getItem(streakKey);
-            if (streakJson != null) {
-              const streakData = JSON.parse(streakJson);
-              streak = streakData.streak;
-            }
-        } else {
-            const streakData = localStorage.getItem(infiniteCount);
-            if (streakData != null) streak = parseInt(streakData);
-        }
+      switch (mode) {
+
+        case "daily":
+          const streakJson = localStorage.getItem(streakKey);
+          if (streakJson != null) {
+            const streakData = JSON.parse(streakJson);
+            streak = streakData.streak;
+          }
+          break;
+
+        case "infinite":
+          const streakData = localStorage.getItem(infiniteCount);
+          if (streakData != null) {
+            streak = parseInt(streakData);
+          }
+          break;
+
       }
-      newCounts[index] = streak;
+
+      newCounts[mode] = streak;
     })
 
+    console.log(newCounts);
     setCounts(newCounts);
   }, [])
 
   return (
     <section id="home">
-      <hgroup>
-        <div className="logo">
-          <SvgLogo/>
-          <h1>POKESORT</h1>
-        </div>
-        <p>{t('home.subtitle')}</p>
-      </hgroup>
-      <nav>
-        {pages.map((page: Page, index: number) => {
-          let StreakCount = <></>
+      
+      <main>
+        <hgroup>
+          <div className="logo">
+            <SvgLogo/>
+            <h1>POKESORT</h1>
+          </div>
+          <p>{t('home.subtitle')}</p>
+        </hgroup>
 
-          if (page.countType != undefined && counts[index] > 0) {
-            StreakCount = (
-              <div>
-                {page.countType != 'count' ?
-                  <StreakIcon />
-                :
-                  <TickIcon />
-                }
-                <span>{counts[index]}</span>
-              </div>
-            )              
-          }
+        <HomeCard
+          page="daily"
+          count={counts["daily"]}
+          href="/daily"
+          sprite={dailySprite} />
+        <HomeCard
+          page="archive"
+          href="/archive"
+          sprite={archiveSprite} />
 
-          return (
-            <Link key={index} href={page.route}>
-              <hgroup className="card-title">
-                <h3>{page.label}</h3>
-                {StreakCount}
-              </hgroup>
-              <p>{page.description}</p>
-              {page.icon}
-            </Link>
-          )
-        })}
-      </nav>
-      <p id="disclaimer">
-        {t(`home.disclaimer`)}
-      </p>
+        <h2>{t(`home.modes`)}</h2>
+        <HomeCard
+          page="infinite"
+          count={counts["infinite"]}
+          href="/infinite"
+          sprite="235" />
+
+        <h2>{t(`home.extras`)}</h2>
+        <HomeCard
+          page="dex"
+          href="/dex"
+          sprite="479" />
+        <HomeCard
+          page="about"
+          href="/about"
+          sprite="441" />
+
+        <p id="disclaimer">
+          {t(`home.disclaimer`)}
+        </p>
+      </main>
+
     </section>
   );
 }
