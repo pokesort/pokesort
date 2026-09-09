c5a457078eb84c1e3aec8c677690e3affc21235d
welsondeivid <53789831+welsondeivid@users.noreply.github.com>
2026-09-09 10:50:38 -0300

    :bug: fix: Divisão por 0 no tratamento de mes quando tentar pegar o pokemon diario

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -145,14 +145,14 @@ export const parseChallengeSelectFields = () => {
   let fields = CHALLENGE_FIELDS;
   let result = {};
   let previous = [];
-  Object.keys(fields).forEach(challenge => {  
-      result[challenge] = Object.keys(fields[challenge]).filter(e => e=="categories"||!previous.includes(e));
-      previous = [...previous, ...result[challenge]];
+  Object.keys(fields).forEach(challenge => {
+    result[challenge] = Object.keys(fields[challenge]).filter(e => e == "categories" || !previous.includes(e));
+    previous = [...previous, ...result[challenge]];
 
-      var categoryIndex = result[challenge].indexOf("categories");
-      if (categoryIndex !== -1) {
-        result[challenge][categoryIndex] = CHALLENGE_CATEGORIES[challenge]
-      }
+    var categoryIndex = result[challenge].indexOf("categories");
+    if (categoryIndex !== -1) {
+      result[challenge][categoryIndex] = CHALLENGE_CATEGORIES[challenge]
+    }
   });
 
   return result;
@@ -316,14 +316,14 @@ export function shuffleArray(array) {
 export function generateTips(ids, queries) {
   const pokemons = pickRandomMult(ids, Math.floor(ids.length / 2));
   const isCustomGroup = queries[0] != "?";
-  if (!isCustomGroup) {    
+  if (!isCustomGroup) {
     queries = queries.slice(1);
   }
   queries = queries.split('&');
-  
+
   const text = queries.reduce((best, cur) => {
     return (WEIGHT_VALUES[cur.split("=")[0]] ?? 0) >
-          (WEIGHT_VALUES[best.split("=")[0]] ?? 0)
+      (WEIGHT_VALUES[best.split("=")[0]] ?? 0)
       ? cur
       : best;
   });
@@ -373,27 +373,91 @@ export const getPuzzleStatus = (ids) => {
   let data = null;
 
   ids.forEach(id => {
-      const userData = localStorage.getItem(`s_${id}`);
-      if (!userData) return;
+    const userData = localStorage.getItem(`s_${id}`);
+    if (!userData) return;
 
-      if (!data) data = {status: -1};
-      
-      const parsed = JSON.parse(userData);
-      if (parsed.shiny != undefined && parsed.shiny.length > 0) {
-          data["shiny"] = parsed.shiny;
-      }
-      if (parsed.status > data.status) {
-          data["status"] = parsed.status;
-      }
+    if (!data) data = { status: -1 };
+
+    const parsed = JSON.parse(userData);
+    if (parsed.shiny != undefined && parsed.shiny.length > 0) {
+      data["shiny"] = parsed.shiny;
+    }
+    if (parsed.status > data.status) {
+      data["status"] = parsed.status;
+    }
   })
 
   return data;
 }
 
+//Gerar a partir de uma formula, precisa tratar mes e ano
 export const getDailyPokemon = () => {
   const now = new Date();
+  // const now = new Date("2026-12-31T12:00:00Z");
 
-  let id = `${Math.ceil((now.getYear()*10)/now.getMonth())+now.getDate()}`
+  let id = `${Math.ceil((now.getYear() * 10) / (now.getMonth() + 1)) + now.getDate()}`
+
+  // console.log(`Current date: ${now.toISOString()}`);
+
+  // console.log("getYear():", now.getYear());
+
+  // console.log("getYear() * 10:", now.getYear() * 10);
+
+  // console.log("getMonth():", now.getMonth() + 1);
+
+  // console.log("(getYear() * 10) / getMonth():", (now.getYear() * 10) / (now.getMonth() + 1));
+
+  // console.log("Math.ceil(...):", Math.ceil((now.getYear() * 10) / (now.getMonth() + 1)));
+
+  // console.log("getDate():", now.getDate());
+
+  // console.log(
+  //   "Resultado final:",
+  //   Math.ceil((now.getYear() * 10) / (now.getMonth() + 1)) + now.getDate()
+  // );
+  // console.log(`Generated daily puzzle ID: ${id}`);
 
   return id;
 }
+
+//Gerar pokemon desde o nascimento do jogo (1 a 1025) e reiniciar no dia seguinte (Pode ser no meio do ano)
+// export const getDailyPokemon = () => {
+
+//   const startDate = new Date(2026, 0, 1);
+//   const now = new Date();
+
+//   const millisecondsPerDay = 1000 * 60 * 60 * 24;
+
+//   const daysSinceStart = Math.floor(
+//     (now.getTime() - startDate.getTime()) / millisecondsPerDay
+//   );
+
+//   const id = `${(daysSinceStart % 1025) + 1}`;
+
+//   console.log("Current date:", now.toISOString());
+//   console.log("Start date:", startDate.toISOString());
+//   console.log("Days since 01/01/2026:", daysSinceStart);
+//   console.log("Generated daily puzzle ID:", id);
+
+//   return id;
+// }
+
+//Gerar sempre o mesmo ao longo do ano (1 a 365) e reiniciar no próximo ano
+// export const getDailyPokemon = () => {
+//   // const now = new Date();
+//   const now = new Date("2026-12-31T12:00:00Z");
+
+//   const startOfYear = new Date(now.getFullYear(), 0, 1);
+
+//   const id = `${Math.floor(
+//     (now.getTime() - startOfYear.getTime()) / (1000 * 60 * 60 * 24)
+//   ) + 1}`;
+
+//   // const id = dayOfYear.toString().padStart(4, "0");
+
+//   console.log("Current date:", now.toISOString());
+//   // console.log("Day of year:", dayOfYear);
+//   console.log("Generated daily puzzle ID:", id);
+
+//   return id;
+// }
